d3f4400893c3c9fa2d3a54481eeda86a3d98feb6
mseroglu <mseroglu47@hotmail.com>
2024-12-14 21:17:50 +0300

    Login butonu tasarım hatası düzeltildi

--- src/components/LoginBtn/index.jsx
+++ src/components/LoginBtn/index.jsx
@@ -31,7 +31,7 @@ const LoginBtn = ({ className, setIsOpen }) => {
          {
             JSON.parse(localStorage.getItem("user"))?.email &&
             <Link to={"/dashboard"} onClick={categoryReset}
-               className="text-slate-600 transition hover:underline text-xs font-semibold whitespace-nowrap " >
+               className="text-slate-600 transition hover:underline text-xs font-semibold whitespace-nowrap  " >
                Yönetici Paneli
             </Link>
          }
@@ -40,17 +40,21 @@ const LoginBtn = ({ className, setIsOpen }) => {
                ? (
                   <Link to={"/"} onClick={handleSignOut}
                      className="text-sm border-2 rounded-md w-30 border-zinc-500 grid place-items-center transition overflow-hidden group">
-                     <div className="text-center text-xs w-full whitespace-nowrap overflow-hidden px-1 flex bg-yellow-400 group-hover:bg-slate-900 group-hover:text-yellow-400 transition"> {JSON.parse(localStorage.getItem("user"))?.name}
+
+                     <div className="justify-center text-xs w-full whitespace-nowrap overflow-hidden px-1 flex bg-yellow-400 group-hover:bg-slate-900 group-hover:text-yellow-400 transition capitalize border-b">
+                        {JSON.parse(localStorage.getItem("user"))?.name}
                      </div>
-                     <div className="flex w-full items-center justify-center gap-2 ps-2 text-black bg-yellow-400 "> 
+
+                     <div className="flex w-full items-center justify-center gap-2 px-2 text-black bg-yellow-400 ">
                         <BiLogOut className="text-lg bg-yellow-400 text-black rounded-full group-hover:scale-105 transition" />
-                        Çıkış 
+                        Çıkış
                      </div>
+
                   </Link>
                ) : (
                   <Link to={"/login"} onClick={() => setIsOpen(false)}
                      className="border-2 rounded-md w-20 border-zinc-500 flex items-center gap-1 justify-center transition hover:bg-slate-900 hover:text-white" >
-                        <BiLogIn />
+                     <BiLogIn />
                      Giriş
                   </Link>
                )
