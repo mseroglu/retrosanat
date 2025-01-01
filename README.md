d317edc354bd491500fb5d4e78eb47866e02e5c6
mseroglu <mseroglu47@hotmail.com>
2025-01-01 23:36:37 +0300

    Carousel rengi yenilendi

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 -  Kampanyalı ürün listeleme
 -  Ürün listeleme pagination yap
--  Anasayfada yeni ürünleri gösterme
 -  Sayfa kapatılınca localeden user verisi silinecek.
 
 
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -13,11 +13,11 @@ function Carousell({ products }) {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect} className='my-2'>
+    <Carousel activeIndex={index} onSelect={handleSelect} className='my-2 bg-slate-800 rounded-md'>
       {products?.map(item => (
         <Carousel.Item key={item.id} onClick={() => navigate("/product/detail/" + item.id)} className='cursor-pointer '>
           <img src={item.photos && item.photos[+item["indexMainImage"]]} alt="product-image"
-            className="w-full max-sm:h-[300px] h-[500px] object-contain rounded-md bg-zinc-600" />
+            className="w-full max-sm:h-[300px] h-[500px] object-contain rounded-md " />
         </Carousel.Item>
       ))}
 
